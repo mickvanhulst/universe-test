79e2164685d60653c12fda362561de9f1fbe8bb2
Mick <mick.vanhulst@gmail.com>
2016-12-14 16:38:46 +0100

    init commit

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ import random
 env = gym.make('flashgames.CoasterRacer-v0')
 observation_n = env.reset()
 
-
-actions = ['ArrowRight', 'ArrowLeft']
 n = 0
-start = False
-prev_score = []
-prev_score.append(-1)
 j = 0
 total_sum = 0
 prev_total_sum = 0
@@ -31,18 +26,18 @@ while n < 10000:
 					event = 'ArrowLeft'
 				prev_event = event
 
+				# Combining events does not work well for me.. :(
 				action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n]
 				env.step([[('KeyEvent', event, True)] for ob in observation_n])
+				#action_n = [[('KeyEvent', 'ArrowUp', True), ('KeyEvent', event, True)] for ob in observation_n]
 				turn = False
 	else:
-
+		# If no turn is needed, go straight
 		action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n]
 
 	observation_n, reward_n, done_n, info = env.step(action_n)
-	#print('reward: ' + str(total_sum) + ' keyEvent: ' + str(action_n) + ' n: ' + str(n))
 
-
-	#for every ten iterations, sum total observations if lower then change
+	#for every 15 iterations, sum total observations if lower then change direction
 	if((j >= 15) and (observation_n[0] != None)):
 		j = 0
 		if((prev_total_sum > total_sum) and turn == False):
@@ -56,13 +51,5 @@ while n < 10000:
 	if(observation_n[0] != None):
 		j+=1
 		total_sum += reward_n[0]
-	
-
-	#Always press forward
-
-	#If user sees object try to go around
-
-	#if user goes to the left of the road it means there is a turn so 
-	# the bot should always try to stay in the middle
 
 	env.render()

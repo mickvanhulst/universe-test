aea2108eed816149566cd9abf1dfeab6e3f4ef1d
Mick <mick.vanhulst@gmail.com>
2016-12-15 12:29:04 +0100

    Finished proproject

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import universe
 import random
 
 def determine_turn(turn, observation_n, j, total_sum, prev_total_sum, reward_n):
-	#for every 15 iterations, sum total observations if lower then change direction
+	# For every 15 iterations, sum total observations if lower then change direction
+	# This makes it more accurate since the iterations are very fast.
 	if(j >= 20):
 		j = 0
 		if((prev_total_sum >= total_sum)):
@@ -23,22 +24,28 @@ def determine_turn(turn, observation_n, j, total_sum, prev_total_sum, reward_n):
 
 
 def main():
+
+	# Init environment
 	env = gym.make('flashgames.CoasterRacer-v0')
 	observation_n = env.reset()
 
+	# Init starting variables
 	n = 0
 	j = 0
 	total_sum = 0
 	prev_total_sum = 0
 	turn = False
 
+	# Init keys
 	left = [('KeyEvent', 'ArrowUp', True),('KeyEvent', 'ArrowLeft', True), ('KeyEvent', 'ArrowRight', False)]
 	right = [('KeyEvent', 'ArrowUp', True),('KeyEvent', 'ArrowLeft', False), ('KeyEvent', 'ArrowRight', True)]
 	forward = [('KeyEvent', 'ArrowUp', True),('KeyEvent', 'ArrowRight', False),('KeyEvent', 'ArrowLeft', False)]
 	
+	# Run while True
 	while True:
 		n += 1
-
+		
+		# If at least one iteration has been made, check if turn is needed.
 		if(n > 1):
 			if(observation_n[0] != None):
 				prev_score = reward_n[0]
@@ -52,11 +59,11 @@ def main():
 			# If no turn is needed, go straight
 			action_n = [forward for ob in observation_n]
 
+		# If there is an observation, game has started and check if turn is needed or ont
 		if(observation_n[0] != None):
 			turn, j, total_sum, prev_total_sum = determine_turn(turn, observation_n[0], j, total_sum, prev_total_sum, reward_n[0])
-			print(turn)
 
-		print(action_n)
+		# Save new variables for each iterations
 		observation_n, reward_n, done_n, info = env.step(action_n)
 
 		env.render()
